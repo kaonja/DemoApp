daed1896f75305102ec3cdf2f607c5beb4870042
charlie <charlielipingu@gmail.com>
2021-01-09 12:15:36 +0300

    new files

---
+++ src/Voucher.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import update from 'react-addons-update';
+
+
+
+class Voucher extends Component {
+    constructor() {
+        super(...argments);
+        this.state = { 
+            originalPassengers: [
+                'Charlie, Farie Da Silva',
+                'David Kipeta, Alphonce John'
+            ],
+            ticket: {
+                company: 'Dalta',
+                flightNo :'0234',
+                departure: {
+                    Airport: 'Dodoma',
+                    time: '2021-01-10T10:00:00.000Z'
+                },
+                arrival: {
+                    aiport: 'Dar Es Salaam',
+                    time: '2021-01-10T11:00:00:00.000Z'
+                },
+                codeShare : [
+                    {company:'GL',flightNo:'9840'},
+                    {company: 'TM',flightNo:'5010'}
+                ]
+
+            }
+
+         }
+    }
+
+
+    render() { 
+        let updatedcodeShare = update(
+            originalPassengers, {
+                codeShare : {
+                    1 : {  $set : { company: 'Fl', flightNo : '20293'} } 
+                }
+            }
+        )
+        this.setState({codeShare: updatedcodeShare })
+        
+        let newArrival = update(arrival, {
+            0: {
+                $set: {
+                    aiport: 'Mwanza',
+                    time : '21kdaj;akd'
+                }
+            }
+        })
+
+        this.setState({arrival : newArrival})
+
+
+        
+
+        return ( 
+
+         );
+    }
+}
+ 
+export default Voucher;
+
+
+
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+import Voucher from './Voucher';
+
+
+
+ReactDOM.render(<Voucher />, document.getElementById('root'))
